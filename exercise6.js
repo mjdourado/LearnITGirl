1e1c83ef68d54bfb368c1de21ae3c1fea7614b4a
Barry Carter <carter.barry@gmail.com>
2019-03-30 07:13:09 -0600

    Altered form

---
+++ exercise6.js
@@ -0,0 +1,15 @@
+function booleans(firstName) {
+    return (person.firstName === "John");
+}
+
+let person = [];
+person[0] = {firstName: "Sam", lastName: "Smith", Age: 32}
+person[1] = {firstName: "Samuel", lastName: "Baker", Age: 32}
+person[2] = {firstName: "Bob", lastName: "Baker", Age: 32}
+person[3] = {firstName: "John", lastName: "Baker", Age: 32}
+person[4] = {firstName: "Jim", lastName: "Baker", Age: 32}
+person[5] = {firstName: "John", lastName: "Smith", Age: 32}
+
+for (let i=0; i < person.length; i++) {
+    console.log(booleans(person[i]));
+}
